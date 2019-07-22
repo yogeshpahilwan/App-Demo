ff33567fd3e25e45b65fc1923dbff4f46871d231
yogeshpahilwan <41663558+yogeshpahilwan@users.noreply.github.com>
2019-07-22 15:41:50 +0200

    Updated

---
+++ index.js
@@ -0,0 +1,16 @@
+var express   = require('express');
+var app       = express();
+var bodyParser= require('body-parser');
+
+//parse application/json
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/',(req,res)=>{
+    res.status(200).send('Hello World !!!');
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log(`Server running on ${port}`);
+});
